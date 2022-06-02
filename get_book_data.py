56fc707b4add3bc534854268ed9bb1d500a98b06
tigerManBot <adamanwar17@gmail.com>
2022-06-02 14:17:08 -0700

    Starting the second file which will gather book data

---
+++ get_book_data.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from time import sleep
+import sys
+
+
+def get_book_name():
+    """Returns the book name as a list as each word is needed separately for the get url function.
+    This can be given either by command line arguments or by temrinal input if no command line argument was given."""
+
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
+
+    print()
